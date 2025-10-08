42a320b42b4a98f56ef585b50df148bb24c9ccb9
tae <tae@taes-MacBook-Air.local>
2025-10-08 20:59:59 +0200

    push

--- src/model/model.mjs
+++ src/model/model.mjs
@@ -23,9 +23,26 @@ export class HashMap {
     const hashSliced = hash.digest("hex").slice(0, 8);
     const hashConvertedToInt = parseInt(hashSliced, 16);
     return hashConvertedToInt % this.size;
-  }
+}
 
-  set(key, value) {
+    rehash() {
+    const previous = this.table;
+    this.size *= 2;
+    this.table = [];
+
+    for (let i = 0; i < this.size; i++) {
+        this.table[i] = [];
+    }
+
+    this.count = 0;
+
+    for (let bucket of previous) {
+        for (let [key, value] of bucket) {
+        this.set(key, value);
+            }
+        }
+    }
+set(key, value) {
     const index = this.hash(key);
     const bucket = this.table[index];
     let updated = false;
@@ -74,22 +91,5 @@ export class HashMap {
     return this.count / this.size;
   }
 
-  rehash() {
-    const previous = this.table;
-    this.size *= 2;
-    this.table = [];
-
-    for (let i = 0; i < this.size; i++) {
-        this.table[i] = [];
-    }
-
-    this.count = 0;
-
-    for (let bucket of previous) {
-        for (let [key, value] of bucket) {
-        this.set(key, value);
-        }
-    }
-    }
 
 }
